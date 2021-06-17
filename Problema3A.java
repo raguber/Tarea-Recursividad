24c9926ae6ee0f1c0fc1d185e98c65a1e4e26e68
JJBV-UCR <81998140+JJBV-UCR@users.noreply.github.com>
2021-06-17 09:51:41 -0600

    Update Problema3A.java

--- Problema3A.java
+++ Problema3A.java
@@ -2,7 +2,7 @@
 /**
  * Solución del problema 3
  *
- * @author (Juan José Bermúdez Vargas & Randy Aguero B90082)
+ * @author (Juan José Bermúdez Vargas  B71082 & Randy Aguero B90082)
  * @version (17.06.2021)
  */
 
